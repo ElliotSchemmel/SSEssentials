d907e8254739d208ad170dd8625fadcb235373cc
regulated <eschem58@gmail.com>
2021-08-31 21:59:08 -0500

    changed Assignment1

--- Java-Basics-1/com/ss/jb1/one/Assignment1.java
+++ Java-Basics-1/com/ss/jb1/one/Assignment1.java
@@ -4,6 +4,9 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 	
+		StringBuilder dots = new StringBuilder("........");
+	
+		// pattern 1
 		System.out.println("1)");
 		for (int i = 1; i <= 4; i++) {
 			for (int j = 0; j < i; j++) {
@@ -11,10 +14,13 @@ public class Assignment1 {
 			}
 			System.out.print('\n');
 		}
-		System.out.println(".........");
+		dots.append('.');
+		System.out.println(dots);
 		
+		// pattern 2
 		System.out.println("2)");
-		System.out.println("..........");		
+		dots.append('.');
+		System.out.println(dots);
 		for (int i = 1; i <= 4; i++) {
 			for (int j = 5; j > i; j--) {
 				System.out.print('*');
@@ -22,19 +28,37 @@ public class Assignment1 {
 			System.out.print('\n');
 		}
 		
+		// pattern 3
 		System.out.println("3)");
-		System.out.println("     *");
-		System.out.println("    ***");
-		System.out.println("   *****");
-		System.out.println("  *******");
-		System.out.println("...........");
+		for (int i = 1; i <= 4; i++) {
+			// print out spaces
+			for (int j = 5; j >= i; j--) {
+				System.out.print(' ');
+			}
+			// print out *'s
+			for (int j = 0; j < i * 2 - 1; j++) {
+				System.out.print('*');
+			}
+			System.out.print('\n');
+		}
+		dots.append('.');
+		System.out.println(dots);
 		
+		// pattern 4
 		System.out.println("4)");
-		System.out.println("............");
-		System.out.println("  *******");	
-		System.out.println("   *****");
-		System.out.println("    ***");
-		System.out.println("     *");
+		dots.append('.');
+		System.out.println(dots);
+		for (int i = 1; i <= 4; i++) {
+			// print out spaces
+			for (int j = 0; j <= i; j++) {
+				System.out.print(' ');
+			}
+			// print out *'s
+			for (int j = 0; j < 9 - (i * 2); j++) {
+				System.out.print('*');
+			}
+			System.out.print('\n');
+		}
 	
 	}
 }

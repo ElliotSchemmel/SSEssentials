2cc5f5233ddb8c760fcde30aca616522bd2b90cd
regulated <eschem58@gmail.com>
2021-09-02 11:02:46 -0500

    added exception handling to Jb2 assignment 1

--- Java-Basics-2/src/com/ss/jb2/one/Assignment1.java
+++ Java-Basics-2/src/com/ss/jb2/one/Assignment1.java
@@ -3,6 +3,7 @@
  */
 package com.ss.jb2.one;
 
+
 /**
  * @author Elliot
  *
@@ -16,14 +17,19 @@ public class Assignment1 {
 	 */
 	public static void main(String[] args) {
 
-		int sum = 0;
+		float sum = 0;
 		
 		for (int i = 0; i < args.length; i++) {
-			sum += Integer.parseInt(args[i]);
+			try {
+				sum += Float.parseFloat(args[i]);
+			}
+			catch (NumberFormatException e) {
+				System.out.println("Error: must enter valid numbers");
+				return;
+			}
 		}
 
 		System.out.println(sum);
-		
 	}
 
 }
